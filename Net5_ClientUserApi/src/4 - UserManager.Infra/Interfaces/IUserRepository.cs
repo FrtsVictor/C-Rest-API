9a78a86efcf2bfc425c31ca7667a6c8c1410db22
FrtsVictor <victorfreitas.job@gmail.com>
2021-03-17 21:31:11 -0300

    Fixed string param

--- Net5_ClientUserApi/src/4 - UserManager.Infra/Interfaces/IUserRepository.cs
+++ Net5_ClientUserApi/src/4 - UserManager.Infra/Interfaces/IUserRepository.cs
@@ -8,6 +8,6 @@ namespace UserManager.Infra.Interfaces
     {
          Task<User> GetByEmail(string email);
          Task<List<User>> SearchByEmail(string email);
-         Task<List<User>> SearchByName(string email);
+         Task<List<User>> SearchByName(string name);
     }
 }
