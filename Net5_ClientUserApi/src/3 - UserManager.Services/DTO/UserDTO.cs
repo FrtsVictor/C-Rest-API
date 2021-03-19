6b587c56a8426e291483781d16931abc136d58cf
FrtsVictor <victorfreitas.job@gmail.com>
2021-03-18 23:29:31 -0300

    Added UserDTO

---
+++ Net5_ClientUserApi/src/3 - UserManager.Services/DTO/UserDTO.cs
@@ -0,0 +1,22 @@
+namespace UserManager.Services.DTO
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }               
+        public string Name { get;  set; }       
+        public string Email { get; set; }
+        public string Password { get;  set; }
+        public string Username { get;  set; }
+
+        public UserDTO()
+        {}
+        public UserDTO(long id, string name, string email, string password, string username)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Username = username;
+        }
+    }
+}
