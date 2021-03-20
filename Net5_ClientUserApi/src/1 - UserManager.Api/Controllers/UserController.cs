251100e55f3705a67f0d3a703e9c18e93d653273
FrtsVictor <victorfreitas.job@gmail.com>
2021-03-20 00:16:28 -0300

    Added Create route

--- Net5_ClientUserApi/src/1 - UserManager.Api/Controllers/UserController.cs
+++ Net5_ClientUserApi/src/1 - UserManager.Api/Controllers/UserController.cs
@@ -1,7 +1,30 @@
-namespace 1___UserManager.Api.Controllers
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManager.Api.Controllers
 {
-    public class UserController
+    [ApiController]
+    public class UserController : ControllerBase
     {
         
+        [HttpPost]
+        [Route("/api/v1")] //sempre versionar rotas
+        public async Task<IActionResult> Create ()
+        {
+            try
+            {
+                return Ok()       ;
+            }
+            // catch (DomainException ex)
+            // {
+            //      // TODO
+            // }
+            catch(Exception)
+            {
+                return StatusCode(500, "error");
+            }
+        }
+
     }
 }
