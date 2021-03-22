cdf52648d567aa71e541d35c1a80352aad4d6e28
FrtsVictor <victorfreitas.job@gmail.com>
2021-03-21 23:56:49 -0300

    Added mapper and repository

--- Net5_ClientUserApi/src/1 - UserManager.Api/Controllers/UserController.cs
+++ Net5_ClientUserApi/src/1 - UserManager.Api/Controllers/UserController.cs
@@ -1,28 +1,50 @@
 using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using UserManager.Api.Utils;
+using UserManager.Api.ViewModels;
+using UserManager.Core.Exceptions;
+using UserManager.Services.DTO;
+using UserManager.Services.Interfaces;
 
 namespace UserManager.Api.Controllers
 {
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper, IUserService userService)
+        {
+            _mapper = mapper;
+            _userService = userService; 
+        }
         
         [HttpPost]
         [Route("/api/v1")] //sempre versionar rotas
-        public async Task<IActionResult> Create ()
+        public async Task<IActionResult> Create ([FromBody] CreateUserViewModel userViewModel)
         {
             try
             {
-                return Ok()       ;
+                var userDTO = _mapper.Map<UserDTO>(userViewModel);
+
+                var userCreated = await _userService.Create(userDTO); // service só vai ler os dtos, por isso o mapper
+
+                return Ok(new ResultViewModel{
+                    Message = "User created sucessfully",
+                    Sucess = true,
+                    Data = userCreated
+                });
             }
-            // catch (DomainException ex)
-            // {
-            //      // TODO
-            // }
+            catch (DomainException ex)
+            {
+                 return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
             catch(Exception)
             {
-                return StatusCode(500, "error");
+                return StatusCode(500, Responses.ApplicationErrorMessage());
             }
         }
 
