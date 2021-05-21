47b2ef5df9d4725ace88a1ffcf950ff37d64bd3d
FrtsVictor <victorfreitas.job@gmail.com>
2021-05-20 22:34:09 -0300

    Added JWT manually Authentication tests

--- Net5_CoursesApi/Controllers/UserController.cs
+++ Net5_CoursesApi/Controllers/UserController.cs
@@ -1,9 +1,8 @@
-using System.Linq;
 using Courses.Filters;
 using Courses.Models.Users;
 using Microsoft.AspNetCore.Mvc;
+using Net5_CoursesApi.Token;
 using Swashbuckle.AspNetCore.Annotations;
-
 namespace Courses.Controllers
 {
     [Route("api/v1/user")]
@@ -18,14 +17,25 @@ namespace Courses.Controllers
         [CustomValidateModelState]
         public IActionResult Login(LoginViewModelInput loginViewModelInput)
         {
-            return Ok(loginViewModelInput);
+           var tokenGenerator = new TokenGenerator();
+           var user = tokenGenerator.createTesteUser();
+           string token =tokenGenerator.GenerateToken(user);
+
+            return Ok(new
+            {
+                Token = token,
+                User = user
+            });
         }
 
         [HttpPost]
+        [SwaggerResponse(statusCode: 201, description: "Created!", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Invalid fields.", Type = typeof(ValidateFields))]
+        [SwaggerResponse(statusCode: 500, description: "Internal server error.", Type = typeof(GenericError))]
         [Route("register")]
-        public IActionResult Register(RegisterViewModelInput loginViewModelInput)
+        public IActionResult Register(RegisterViewModelInput registerViewModelInput)
         {
-            return Created("", loginViewModelInput);
+            return Created("", registerViewModelInput);
         }
 
     }
