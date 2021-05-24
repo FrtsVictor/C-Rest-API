ad421d8a4fc6bbfd05b103fd1d75b0c3d24718d3
FrtsVictor <victorfreitas.job@gmail.com>
2021-05-23 23:58:01 -0300

    Added custom mapping

--- Net5_CoursesApi/src/Infra/Context/Mappings/UserMapping.cs
+++ Net5_CoursesApi/src/Infra/Context/Mappings/UserMapping.cs
@@ -9,12 +9,37 @@ namespace Infra.Context.Mappings
         public void Configure(EntityTypeBuilder<User> builder)
         {
             builder.ToTable("users");
+
             builder.HasKey(p => p.Id);
-            builder.Property(p => p.Id).ValueGeneratedOnAdd();
-            builder.Property(p => p.Name);
-            builder.Property(p => p.Username);
-            builder.Property(p => p.Email);
-            builder.Property(p => p.Password);
+
+            builder.Property(p => p.Id)
+               .HasColumnName("id")
+               .ValueGeneratedOnAdd()
+               .IsRequired();
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("name")
+                .HasColumnType("VARCHAR(50)");
+
+            builder.Property(p => p.Username)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnName("username")
+                .HasColumnType("VARCHAR(30)");
+
+            builder.Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(120)
+                .HasColumnName("email")
+                .HasColumnType("VARCHAR(120)");
+
+            builder.Property(p => p.Password)
+                .IsRequired()
+                .HasMaxLength(80)
+                .HasColumnName("password")
+                .HasColumnType("VARCHAR(80)");
         }
     }
 }
