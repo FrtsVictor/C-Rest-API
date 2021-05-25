f6753b45d7cf047c898ec475b8aac81de2ec7470
FrtsVictor <victorfreitas.job@gmail.com>
2021-05-24 23:50:32 -0300

    Added UserService

--- Net5_CoursesApi/src/App/Services/IUserService.cs
+++ Net5_CoursesApi/src/App/Services/IUserService.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using App.Models.Users;
+using Courses.Models.Users;
 using Infra.Entities;
+using Net5_CoursesApi.Models.Users;
 
 namespace App.Services
 {
     public interface IUserService
     {
-         Task<User> Criar(User user);
+        Task<UserViewModelOutput> Create(RegisterViewModelInput registerViewModelInput);
+        Task<UpdateUserViewModel> Update(UpdateUserViewModel updateUserViewModel);
+        Task Remove(long id);
+        Task<UserViewModelOutput> Get(long id);
+        Task<List<UserViewModelOutput>> Get();
+        Task<List<UserViewModelOutput>> SearchByEmail(string email);
+        Task<UserViewModelOutput> GetByUsername(string email);
     }
 }
--- Net5_CoursesApi/src/App/Services/UserService.cs
+++ Net5_CoursesApi/src/App/Services/UserService.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using App.Models.Users;
+using Courses.Models.Users;
 using Infra.Entities;
 using Infra.Repositories.Interfaces;
+using Net5_CoursesApi.Models.Users;
 
 namespace App.Services
 {
@@ -13,10 +17,47 @@ namespace App.Services
             _userRepository = userRepository;
         }
                 
-        public async Task<User> Criar(User user)
+        public async Task<User> Create(User user)
         {
             var userCreated = await _userRepository.Create(user);
             return userCreated;
+        }
+
+        public async Task<UserViewModelOutput> Create(RegisterViewModelInput registerViewModelInput)
+        {
+            var user = 
+            var userCreated = await _userRepository.Create();
+            return new UserViewModelOutput();
+        }
+
+        public Task<UserViewModelOutput> Get(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<UserViewModelOutput>> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserViewModelOutput> GetByUsername(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Remove(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<UserViewModelOutput>> SearchByEmail(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UpdateUserViewModel> Update(UpdateUserViewModel updateUserViewModel)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
